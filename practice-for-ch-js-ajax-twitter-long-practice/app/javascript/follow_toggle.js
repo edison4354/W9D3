10ddc5a011146d256dff24dcee6df3d1977d69a3
Edison <edison4354@gmail.com>
2024-05-01 15:05:44 -0700

    Stuck on follow button bug

--- practice-for-ch-js-ajax-twitter-long-practice/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice/app/javascript/follow_toggle.js
@@ -3,6 +3,7 @@ import { API, broadcast } from "./util";
 export default class FollowToggle {
   constructor(toggleButton) {
     // Your code here
+    this.userId = toggleButton.dataset.userId
     this.toggleButton = toggleButton;
     this.toggleButton.addEventListener("click", this.handleClick.bind(this))
   }
@@ -22,18 +23,35 @@ export default class FollowToggle {
 
   async follow() {
     // Your code here
+    this.followState = 'following'
+    await API.followUser(this.userId)
     this.followState = 'followed'
   }
 
   async unfollow() {
     // Your code here
+    this.followState = 'unfollowing'
+    await API.unfollowUser(this.userId)
     this.followState = 'unfollowed'
   }
 
   render() {
     switch (this.followState) {
       // Your code here
-      
+        case 'followed':
+            this.toggleButton.disable = false;
+            this.toggleButton.innerText = "Unfollow!";
+        case 'unfollewed':
+            this.toggleButton.disable = false;
+            this.toggleButton.innerText = "Follow!";
+        case 'following':
+            this.toggleButton.disable = true;
+            this.toggleButton.innerText = "Following...";
+            break;
+        case 'unfollowing':
+            this.toggleButton.disable = true;
+            this.toggleButton.innerText = "Unfollowing...";
+            break;
     }
   }
 
--- practice-for-ch-js-ajax-twitter-long-practice/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice/app/javascript/util/api.js
@@ -3,11 +3,17 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
-    X-CSRF-Token: csrfToken,
+    'X-CSRF-Token': csrfToken,
+    'Accept': 'application/json',
     ...options.headers
   };
 
-  return await fetch(url, options);
+  const response = await fetch(url, options);
+  if (response.ok) {
+    return response.json(); 
+  } else {
+    throw response
+  }
 }
 
 export function followUser(id){
